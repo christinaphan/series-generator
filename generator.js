ae281adca5d6697291c5997d23aec78559790301
Christina Phan <88528865+christinaphan@users.noreply.github.com>
2022-01-11 14:24:13 -0800

    Added all tests

--- generator.js
+++ generator.js
@@ -1,17 +1,3 @@
-const questions = [
-    "$$\\sum_{n=1}^{\\infty}\\ln(\\frac{n}{n+1})$$",
-    "$$\\sum_{n=1}^{\\infty}\\ln(\\frac{n}{n+2})$$",
-    "$$\\sum_{n=1}^{\\infty}\\ln(\\frac{n}{n+3})$$",
-    "$$\\sum_{n=1}^{\\infty}\\ln(\\frac{n}{n+4})$$",
-];
-
-const answers = [
-    "Converges",
-    "Diverges",
-    "Converges",
-    "Diverges"
-];
-
 const nthTermQuestions = [
     "nthTerm #1 $$a+b=c$$",
     "nthTerm #2 $$a+b=c$$",
@@ -40,15 +26,153 @@ const pseriesAnswers = [
     "pseries #4 diverges",
 ];
 
+const geoQuestions = [
+    "geo #1 $$a+b=c$$",
+    "geo #2 $$a+b=c$$",
+    "geo #3 $$a+b=c$$",
+    "geo #4 $$a+b=c$$",
+];
+
+const geoAnswers = [
+    "geo #1 converges",
+    "geo #2 diverges",
+    "geo #3 converges",
+    "geo #4 diverges",
+];
+
+const compareQuestions = [
+    "compare #1 $$a+b=c$$",
+    "compare #2 $$a+b=c$$",
+    "compare #3 $$a+b=c$$",
+    "compare #4 $$a+b=c$$",
+];
+
+const compareAnswers = [
+    "compare #1 converges",
+    "compare #2 diverges",
+    "compare #3 converges",
+    "compare #4 diverges",
+];
+
+const altQuestions = [
+    "alt #1 $$a+b=c$$",
+    "alt #2 $$a+b=c$$",
+    "alt #3 $$a+b=c$$",
+    "alt #4 $$a+b=c$$",
+];
+
+const altAnswers = [
+    "alt #1 converges",
+    "alt #2 diverges",
+    "alt #3 converges",
+    "alt #4 diverges",
+];
+
+const ratioQuestions = [
+    "ratio #1 $$a+b=c$$",
+    "ratio #2 $$a+b=c$$",
+    "ratio #3 $$a+b=c$$",
+    "ratio #4 $$a+b=c$$",
+];
+
+const ratioAnswers = [
+    "ratio #1 converges",
+    "ratio #2 diverges",
+    "ratio #3 converges",
+    "ratio #4 diverges",
+];
+
+const rootQuestions = [
+    "root #1 $$a+b=c$$",
+    "root #2 $$a+b=c$$",
+    "root #3 $$a+b=c$$",
+    "root #4 $$a+b=c$$",
+];
+
+const rootAnswers = [
+    "root #1 converges",
+    "root #2 diverges",
+    "root #3 converges",
+    "root #4 diverges",
+];
+
+const integralQuestions = [
+    "integral #1 $$a+b=c$$",
+    "integral #2 $$a+b=c$$",
+    "integral #3 $$a+b=c$$",
+    "integral #4 $$a+b=c$$",
+];
+
+const integralAnswers = [
+    "integral #1 converges",
+    "integral #2 diverges",
+    "integral #3 converges",
+    "integral #4 diverges",
+];
+
+const limitQuestions = [
+    "limit #1 $$a+b=c$$",
+    "limit #2 $$a+b=c$$",
+    "limit #3 $$a+b=c$$",
+    "limit #4 $$a+b=c$$",
+];
+
+const limitAnswers = [
+    "limit #1 converges",
+    "limit #2 diverges",
+    "limit #3 converges",
+    "limit #4 diverges",
+];
 
 $(document).ready(function() {
     $('#show').hide();
     $('#generate').on('click', function() {
+        var pos_questions = [];
+        var pos_answers = [];
+        if(nthterm.checked) {
+            pos_questions.push(nthTermQuestions);
+            pos_answers.push(nthTermAnswers);
+        }
+        if(ptest.checked) {
+            pos_questions.push(pseriesQuestions);
+            pos_answers.push(pseriesAnswers);
+        }
+        if(geotest.checked) {
+            pos_questions.push(geoQuestions);
+            pos_answers.push(geoAnswers);
+        }
+        if(comparetest.checked) {
+            pos_questions.push(compareQuestions);
+            pos_answers.push(compareAnswers);
+        }
+        if(altseries.checked) {
+            pos_questions.push(altQuestions);
+            pos_answers.push(altAnswers);
+        }
+        if(ratio.checked) {
+            pos_questions.push(ratioQuestions);
+            pos_answers.push(ratioAnswers);
+        }
+        if(roottest.checked) {
+            pos_questions.push(rootQuestions);
+            pos_answers.push(rootAnswers);
+        }
+        if(integral.checked) {
+            pos_questions.push(integralQuestions);
+            pos_answers.push(integralAnswers);
+        }
+        if(limitcompare.checked) {
+            pos_questions.push(limitQuestions);
+            pos_answers.push(limitAnswers);
+        }
+
         $('#show').show();
-        var rand_problem = Math.floor(Math.random() * questions.length);
-        $('.question-body').html(questions[rand_problem]);
+        var rand_problem_test = Math.floor(Math.random() * pos_questions.length);
+        var rand_problem = Math.floor(Math.random() * pos_questions[rand_problem_test].length);
+        $('.question-body').html(pos_questions[rand_problem_test][rand_problem]);
         $('.answer-body').hide();
-        $('.answer-body').html(answers[rand_problem]);
+        $('.answer-body').html(pos_answers[rand_problem_test][rand_problem]);
+
         MathJax.typeset()
     });
     $('#show').on('click', function() {
